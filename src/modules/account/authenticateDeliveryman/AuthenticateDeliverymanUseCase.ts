cc7d4d4958b8fa75eddea9a77249fade923ae32a
Rythelle C. Pereira <rythelle20@hotmail.com>
2022-01-29 21:11:32 -0300

    Finished Feature Deliveryman

---
+++ src/modules/account/authenticateDeliveryman/AuthenticateDeliverymanUseCase.ts
@@ -0,0 +1,35 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { prisma } from "../../../database/PrismaClient";
+
+interface AuthenticateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateDeliverymanUseCase {
+  async execute({ username, password }: AuthenticateDeliveryman) {
+    const deliveryman = await prisma.deliveryman.findFirst({
+      where: {
+        username,
+      },
+    });
+
+    if (!deliveryman) {
+      throw new Error("Username or password invalid!");
+    }
+
+    const passwordMatch = await compare(password, deliveryman.password);
+
+    if (!passwordMatch) {
+      throw new Error("Username or password invalid!");
+    }
+
+    const token = sign({ username }, String(process.env.JWT_SECRET), {
+      subject: deliveryman.id,
+      expiresIn: "10m",
+    });
+
+    return token;
+  }
+}
